1cea43a25c87293d08a724a126c27eb356eb5266
zhangjie <731255090@qq.com>
2018-06-28 00:25:27 +0800

    数据校验

---
+++ src/main/java/org/spring/dao/jdbcHibernate/UserService.java
@@ -0,0 +1,43 @@
+package org.spring.dao.jdbcHibernate;
+
+import org.spring.mvc.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService {
+    private HibernateTemplate hibernateTemplate;
+    private ScoreService scoreService;
+
+    @Autowired
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate){
+        this.hibernateTemplate = hibernateTemplate;
+    }
+
+    @Autowired
+    public void setScoreService(ScoreService scoreService){
+        this.scoreService = scoreService;
+    }
+
+    public void logon(String userName){
+        //通过Hibernate技术访问数据
+        System.out.println("befor updateLastLogonTime()..");
+        updateLastLogonTime(userName);
+        System.out.println("end updateLastLogonTime()..");
+
+        //通过JDBC技术访问数据
+        System.out.println("before scoreService.addScore()..");
+        scoreService.addScore(userName,20);
+        System.out.println("end socreService.addScore()..");
+    }
+
+    //默认情况下,Hibernate对数据的更改只记录在一级缓存中，等到事务提交或显示调用flush()方法时才会将一级缓存中的数据同步到数据库中
+    public void updateLastLogonTime(String userName){
+        User user = hibernateTemplate.get(User.class,userName);
+        user.setLastLogo;
+        hibernateTemplate.update(user);
+
+        hibernateTemplate.flush();
+    }
+}
